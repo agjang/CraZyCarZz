759ffd4daead479a8cb54b2926b524452c905c3d
John Bestavros <61709626+Johannes2755@users.noreply.github.com>
2022-04-24 15:00:59 -0400

    Updated the previously blank car park class

--- Project_Report_Materials/Car_Park.java
+++ Project_Report_Materials/Car_Park.java
@@ -1,4 +1,12 @@
 
 public class Car_Park {
-
+	private int ID;
+	private String address;
+	private String phone;
+	
+	public Car_Park(int ID, String address, String phone_number) {
+		this.ID = ID;
+		this.address = address;
+		this.phone = phone_number;
+	}
 }
