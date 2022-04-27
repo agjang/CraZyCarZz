f5dc360caa04b780206b050ae08998cc179d987b
John Bestavros <61709626+Johannes2755@users.noreply.github.com>
2022-04-26 22:20:45 -0400

    Add files via upload

--- Code/Car_Park.java
+++ Code/Car_Park.java
@@ -3,10 +3,12 @@ public class Car_Park {
 	private int ID;
 	private String address;
 	private String phone;
+	public int Capacity;
 	
-	public Car_Park(int ID, String address, String phone_number) {
+	public Car_Park(int ID, String address, String phone_number, int bottleneck) {
 		this.ID = ID;
 		this.address = address;
 		this.phone = phone_number;
+		this.Capacity = bottleneck;
 	}
 }
