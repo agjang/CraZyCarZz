0553e52c4513e9f8b04c8038a0bd4c2646483a95
John Bestavros <61709626+Johannes2755@users.noreply.github.com>
2022-04-27 20:29:01 -0400

    Add files via upload

--- Code/Car_Park.java
+++ Code/Car_Park.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 
 public class Car_Park {
 	private int ID;
 	private String address;
 	private String phone;
 	public int Capacity;
+	public ArrayList<Car> NotHiredCars = new ArrayList<Car>();
+	public ArrayList<Car> HiredCars = new ArrayList<Car>();
 	
 	public Car_Park(int ID, String address, String phone_number, int bottleneck) {
 		this.ID = ID;
