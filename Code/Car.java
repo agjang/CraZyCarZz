095aa9b5cbb64d6faf3d10e6c8bfd9b442da00b2
John Bestavros <61709626+Johannes2755@users.noreply.github.com>
2022-04-27 20:27:09 -0400

    Add files via upload

--- Code/Car.java
+++ Code/Car.java
@@ -1,9 +1,9 @@
 public class Car {
-	private int Car_ID;
-	private String model;
+	public int Car_ID;
+	public String model;
 	private String License_plate;
 	private Car_Park Location;
-	private CarStatus status;
+	public CarStatus status;
 	
 	public Car(int ID, String model, String License_plate, Car_Park Location) {
 		this.Car_ID = ID;
