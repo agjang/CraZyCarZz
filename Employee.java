7032ce01802f90f65e8f1b5139ab1f5badce549c
John Bestavros <61709626+Johannes2755@users.noreply.github.com>
2022-04-27 20:27:41 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+
+public class Employee {
+	private int ID;
+	private String name;
+	private int age;
+	public Employee(int ID, String name, int age) {
+		this.ID = ID;
+		this.name = name;
+		this.age = age;
+	}
+}
